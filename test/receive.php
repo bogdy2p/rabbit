936472f69cfc668b92d91e9b909e75cee3140a8e
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-03-23 14:40:12 +0200

    Updates / fixes to every file

--- test/receive.php
+++ test/receive.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('thepbc.go.ro', 5672, 'admin', 'admin123');
 $channel    = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
