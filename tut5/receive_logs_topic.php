30240268dd323c000828b29234bad638cface058
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-03-23 11:22:08 +0200

    Updated tut5 receivelogstopic file

--- tut5/receive_logs_topic.php
+++ tut5/receive_logs_topic.php
@@ -6,19 +6,19 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel    = $connection->channel();
 
-$channel->exchange_declare('direct_logs', 'direct', false, false, false);
+$channel->exchange_declare('topic_logs', 'topic', false, false, false);
 
 list($queue_name,, ) = $channel->queue_declare("", false, false, true, false);
 
-$severities = array_slice($argv, 1);
-if (empty($severities)) {
+$binding_keys = array_slice($argv, 1);
+if (empty($binding_keys)) {
     file_put_contents('php://stderr',
-        "Usage: $argv[0] [info] [warning] [errror] \n");
+        "Usage: $argv[0] [binding_key] \n");
     exit(1);
 }
 
-foreach ($severities as $severity) {
-    $channel->queue_bind($queue_name, 'direct_logs', $severity);
+foreach ($binding_keys as $binding_key) {
+    $channel->queue_bind($queue_name, 'topic_logs', $binding_key);
 }
 
 echo ' [*] Waiting for logs. To exit press ctrlC', "\n";
